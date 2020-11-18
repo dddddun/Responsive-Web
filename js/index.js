2f3a19b0a4fd90260826f048b0014dc2de819f86
Seonghyunsun <leaf1993@naver.com>
2020-11-19 03:09:14 +0900

    Responsive Web page design

---
+++ js/index.js
@@ -0,0 +1,16 @@
+$(function(){
+    $('#project01').click(function(){
+    location.href = 'https://github.com/KJS94';
+    });
+    
+    $('#project02').click(function(){
+    location.href = 'https://github.com/dddddun/ASP.NET-Board';
+    });
+    
+    $('#project03').click(function(){
+    location.href = 'https://github.com/dddddun/Responsive-Web';
+    });    
+});
+
+
+
